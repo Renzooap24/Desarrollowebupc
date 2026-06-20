d9a331d251e2849a2e50340eca92ef99f162dc7d
Leticia <tuemail@gmail.com>
2026-06-20 01:39:08 -0500

    feat: modulo clientes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { ServiciosComponent } from './servicios/servicios';
+import { Clientes } from './clientes/clientes';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: Login },
-  { path: 'servicios', component: ServiciosComponent }
+  { path: 'servicios', component: ServiciosComponent },
+  { path: 'clientes', component: Clientes }
 ];
